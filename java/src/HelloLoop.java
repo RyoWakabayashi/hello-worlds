9bd323e3005872a63f3ca25d3635f0942a756404
RyoWakabayashi <rwakabay@oec.co.jp>
2021-04-26 00:44:27 +0900

    feat: Divide codes

---
+++ java/src/HelloLoop.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.text.MessageFormat;
+
+/**
+ * Say hello.
+ * @author RyoWakabayashi
+ * @version 1.0
+ */
+public final class HelloLoop {
+  private HelloLoop() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  /**
+    * main.
+    * @param args Dummy
+    */
+  public static void main(final String[] args) {
+    ArrayList<String> worldList = new ArrayList<String>();
+    worldList.add("a");
+    worldList.add("b");
+    worldList.add("c");
+
+    String format = "world is {0}!";
+    for (String world: worldList) {
+      System.out.println(MessageFormat.format(format, world));
+    }
+
+    HashMap<String, String> worldMap = new HashMap<String, String>();
+    worldMap.put("a", "aaa");
+    worldMap.put("b", "bbb");
+    worldMap.put("c", "ccc");
+
+    String mapFormat = "world {0} is {1}!";
+    for (HashMap.Entry<String, String> entry: worldMap.entrySet()) {
+      System.out.println(
+        MessageFormat.format(mapFormat, entry.getKey(), entry.getValue()));
+    }
+  }
+}
---
+++ nodejs/src/helloBranch.js
@@ -0,0 +1,20 @@
+const helloWorld = 'Hello, world!'
+
+if (helloWorld === 'Good morning') {
+  console.log('morning')
+} else if (helloWorld.includes('Hello')) {
+  console.log('noon')
+} else {
+  console.log('night')
+}
+
+switch (helloWorld) {
+  case 'Hello, world!':
+    console.log('nonn')
+    break
+  case 'Good morning, world!':
+    console.log('morning')
+    break
+  default:
+    console.log('night')
+}
