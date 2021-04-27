f382e3a6383a41060530d9794f8adfc110569f9f
RyoWakabayashi <rwakabay@oec.co.jp>
2021-04-27 18:34:59 +0900

    chore: tmp

--- rust/src/hello_branch.rs
+++ rust/src/hello_branch.rs
@@ -1,20 +1,17 @@
 fn main() {
-  let hello_world = "Hello, world!";
+    let hello_world = "Hello, world!";
 
-  if hello_world == "Good morning" {
-    println!("morning");
-  } else if hello_world.contains("Hello") {
-    println!("noon");
-  } else {
-    println!("night");
-  }
+    if hello_world == "Good morning" {
+        println!("morning");
+    } else if hello_world.contains("Hello") {
+        println!("noon");
+    } else {
+        println!("night");
+    }
 
-  match hello_world {
-    "Hello, world!" =>
-      println!("nonn"),
-    "Good morning, world!" =>
-      println!("morning"),
-    _ =>
-      println!("night")
-  }
+    match hello_world {
+        "Hello, world!" => println!("nonn"),
+        "Good morning, world!" => println!("morning"),
+        _ => println!("night"),
+    }
 }
